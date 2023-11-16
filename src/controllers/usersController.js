6bdfa490e2a8069b535543c21419337626461ea8
victoriasuarez <victoriasuarez49@gmail.com>
2023-11-16 20:20:04 -0300

    cambios

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,9 +1,54 @@
+const bcrypt = require('bcrypt');
+const fs = require('fs');
+const path = require('path');
+
+const dataPath = path.join(__dirname, '../data/users.json');
+
 const controller = {
     login(req, res) {
         res.render('users/login');
     },
+    showRegisterForm(req,res) {
+        res.render("users/register")
+    },
     register(req, res) {
-        res.render('users/register');
+        console.log('Datos del formulario:', req.body);
+        const { firstName, lastName, email, password, confirPassword, dateOfBirth} = req.body;
+        
+        if (password !== confirPassword) {
+            throw new Error('Las contraseñas no coinciden');
+        }
+
+        if (!password) {
+            throw new Error('La contraseña no puede estar vacía');
+        }
+
+        const jsonData = fs.readFileSync(dataPath, 'utf-8');
+        const data = JSON.parse(jsonData);
+        const existingUser = data.find(user => user.email === email);
+        if(existingUser) {
+            throw new Error('Usuario ya existente. El email ya se encuentra en nuestros registros');
+        }
+
+        const hashedPassword = bcrypt.hashSync(password, 10);
+
+        const imageFileName = req.file ? req.file.filename : "images/users/default-image-users.png"
+
+        const newUser = {
+            id: data.length + 1,
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+            dateOfBirth,
+            image: imageFileName
+        };
+
+        data.push(newUser);
+
+        fs.writeFileSync(dataPath, JSON.stringify(data, null, 2), 'utf-8');
+
+        res.redirect('user/login');
     }
 };
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const usersController = require('../controllers/usersController');
+const upload = require('../config/multerConfig');
 
 const router = express.Router();
 
 router.get('/login', usersController.login);
-router.get('/register', usersController.register);
+router.get('/register', usersController.showRegisterForm);
+router.post('/register', upload.single('image') ,usersController.register);
 
 
 
