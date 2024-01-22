1323a976213262c2e2cf7475178fa8981d4e4f3c
lucasgarcia2297 <lucas.garcia2297@gmail.com>
2024-01-22 16:36:37 -0300

    corrijo la ruta del storage

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.resolve(__dirname, 'public/images/users'));
+        cb(null, path.resolve(__dirname, '../public/images/users'));
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
