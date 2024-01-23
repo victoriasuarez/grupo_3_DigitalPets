7c65fadcb4c362d031e7553d459773c7406cbef7
emapernice <perniceemanuel@gmail.com>
2024-01-23 20:16:29 -0300

    Modifique configuracion Multer

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -6,7 +6,7 @@ const storage = multer.diskStorage({
         cb(null, path.resolve(__dirname, '../public/images/users'));
     },
     filename: function (req, file, cb) {
-        cb(null, file.filename + '-' + Date.now() + path.extname(file.originalname));
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
 });
 
